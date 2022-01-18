7ab3a47e00c830d47396a0c3917f4e6935cd06c2
williamostler <wostler@live.co.uk>
2022-01-18 17:31:40 +0000

    Remove unecessary assignment of function

--- Search/ExcelProcess.py
+++ Search/ExcelProcess.py
@@ -44,5 +44,5 @@ def formatResults(searchID):
     write['H5'] = f'   Total price: £{round(totalprice, 2)}'
     write['H5'].font = Font(size=19)
 
-    results_file = wb.save(f"BOMSearch{searchID}results.xlsx")
+    wb.save(f"/Users/williamostler/Desktop/University/Stage 2/CSC2033 - Software Engineering Team Project/PriceComparisonStockChecker/SearchResults/BOMSearch{searchID}results.xlsx")
     wb.close()
