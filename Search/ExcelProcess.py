96dc2a84d8e0092bb96ccab25698c011e526ef91
williamostler <wostler@live.co.uk>
2022-01-18 14:19:04 +0000

    Add Excel Output functionality

--- Search/ExcelProcess.py
+++ Search/ExcelProcess.py
@@ -1,4 +1,6 @@
+import openpyxl
 from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 from models import Results
 
 def formatResults(searchID):
@@ -7,19 +9,36 @@ def formatResults(searchID):
     results_data = Results.query.filter(Results.searchnumber == searchID).all()
     write = wb.active
 
+
     i=0
     for result in results_data:
 
-        write[f'C{str(8+i)}'] = result.partnumber
-        write[f'D{str(8+i)}'] = result.supplier
-        write[f'E{str(8+i)}'] = result.stock
-        write[f'F{str(8+i)}'] = result.stockrequired
-        write[f'G{str(8+i)}'] = result.priceperunit
-        write[f'H{str(8+i)}'] = result.totalprice
-        write[f'I{str(8+i)}'] = result.link
+        if result.stock <= result.stockrequired or Results.stock == 0:
+
+            write[f'C{str(8+i)}'] = result.partnumber
+            write[f'D{str(8+i)}'] = result.supplier
+            write[f'E{str(8+i)}'] = "Not Found"
+            write[f'F{str(8+i)}'] = "Not Found"
+            write[f'G{str(8+i)}'] = "Not Found"
+            write[f'H{str(8+i)}'] = "Not Found"
+            write[f'I{str(8+i)}'] = "Not Found"
+
+            write[f'E{str(8 + i)}'].fill = PatternFill(patternType='solid', fgColor='00800000')
+
+        else:
+            write[f'C{str(8+i)}'] = result.partnumber
+            write[f'D{str(8+i)}'] = result.supplier
+            write[f'E{str(8+i)}'] = result.stock
+            write[f'F{str(8+i)}'] = result.stockrequired
+            write[f'G{str(8+i)}'] = result.priceperunit
+            write[f'H{str(8+i)}'] = result.totalprice
+            write[f'I{str(8+i)}'] = result.link
+
+            write[f'E{str(8 + i)}'].fill = PatternFill(patternType='solid', fgColor='00008000')
 
         i+=1
 
-    wb.save("results.xlsx")
+    wb.save(f"BOMSearch{searchID}results.xlsx")
+    wb.close()
 
 formatResults(1)
