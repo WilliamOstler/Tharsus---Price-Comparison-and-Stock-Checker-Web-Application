537feb01f5e249ca15ca2c86f8f0e7d58dcc8dfe
Cameron Main <cameroncmain@gmail.com>
2022-01-19 22:27:20 +0000

    Update UI and general fixes

--- users/forms.py
+++ users/forms.py
@@ -15,15 +15,15 @@ class RegisterForm(FlaskForm):
     firstname = StringField(validators=[character_check])
     lastname = StringField(validators=[character_check])
     email = StringField(validators=[DataRequired(), Email()])
-    password = PasswordField(validators=[DataRequired(), Length(min=6, max=12, message='Password must be betweem 6 and 12 characters in length')])
-    confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password', message='Both password fields must be equal')])
+    password = PasswordField(validators=[DataRequired(), Length(min=6, max=12, message='Password must be betweem 6 and 12 characters in length.')])
+    confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password', message='Both password fields must be equal.')])
     submit = SubmitField()
 
 
     def validate_password(self, password):
         pw = re.compile(r'(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}')
         if not pw.match(self.password.data):
-            raise ValidationError("Password must contain at least 1 Digit, 1 Lowercast Character, 1 Uppercase Character and 1 Special Character")
+            raise ValidationError("Password must contain at least 1 Digit, 1 Lowercast Character, 1 Uppercase Character & 1 Special Character.")
 
 
 class LoginForm(FlaskForm):
