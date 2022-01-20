7b366dc88b5a2c31a775c85e0f0efa2fd0c92e6b
NojusIva <N.Ivanauskas1@newcastle.ac.uk>
2022-01-20 17:23:49 +0000

    add additional information to FindChipsScraping.py

--- Search/FindChipsScraping.py
+++ Search/FindChipsScraping.py
@@ -4,25 +4,29 @@ import re
 from Search import DatabaseProcess
 
 
-def stringchecker(value):
+def string_checker(value):
     """
     This method checks if a string could be converted to an int, if it cannot
     then it is converted to a 0.
-    @:returns result
-    @author Nojus Ivanauskas
+    :param value: any string value
+    :returns integer
+    :author Nojus Ivanauskas
 
     """
     try:
         e = int(value)
         result = e
-    except:
+    except ValueError:
         result = 0
     return result
 
 
-def findchipsscraper(partnumber, quantity_required, searchID):
+def findchipsscraper(partnumber, quantity_required, search_id):
     """
-    This method adds arrays of results to the database after scraping the findchips website.
+    This method adds arrays of results to the database after scraping the findchips website
+    :param partnumber: the number of the part that is needed to be scraped
+    :param quantity_required: how many parts are needed
+    :param search_id: the id which was calculated for this search
 
     @author Nojus Ivanauskas
 
@@ -40,21 +44,22 @@ def findchipsscraper(partnumber, quantity_required, searchID):
     for distributor in distributors:
         distributor_name = distributor.find('h3', class_='distributor-title').text.replace(' ', '').replace('\n', '').\
             replace(
-            '\xC2','').replace('\x95', '').replace('ECIA(NEDA)MemberAuthorizedDistributor','').\
-            replace('AuthorizedDistributor','').\
-            replace('Manufacturer Direct – Inventory Available for Immediate and Future Delivery','')
-
+            '\xC2', '').replace('\x95', '').replace('ECIA(NEDA)MemberAuthorizedDistributor', '').\
+            replace('AuthorizedDistributor', '').\
+            replace('Manufacturer Direct – Inventory Available for Immediate and Future Delivery', '').replace(
+            "ECIA(NEDA)", ''
+        )
         for listing in distributor.find_all('tr', class_='row'):
             stock = listing.find('td', class_='td-stock').text
             numeric_string = re.sub("[^0-9]", "", stock)  # removes all non-numeric characters from a string
 
-            if stringchecker(numeric_string) != 0 and stringchecker(numeric_string) >= quantity_required:
+            if string_checker(numeric_string) != 0 and string_checker(numeric_string) >= quantity_required:
                 part_id = listing.find('td', class_='td-part first').text.replace(' ', '').replace('\n', '')
                 link = f"https:{listing.td.div.a['href']}"
                 for price in listing.find_all('li'):
                     quantity = price.find('span', class_='label').text
-                    value = price.find('span', class_='value').text.replace('£','')
+                    value = price.find('span', class_='value').text.replace('£', '')
                     if int(quantity) <= quantity_required:
                         result = [part_source, partnumber, part_id, distributor_name, numeric_string, quantity_required,
-                                         float(value), float(value)*quantity_required,link, searchID]
+                                  float(value), float(value) * quantity_required, link, search_id]
                         DatabaseProcess.addRow(result)
